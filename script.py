2b1888f30817bb543ae4f04dce4c7001bc644457
Sbrussee <Siemenbrussee@hotmail.com>
2023-03-17 11:17:58 +0100

    added pickle load for CE tensor

--- script.py
+++ script.py
@@ -120,9 +120,13 @@ class holonet_pipeline:
 
         #Now we compute the tensor of communication events
         if path.exists("output/ce_tensor_"+self.name+".pkl"):
-            with open("output/ce_tensor_"+self.name+".pkl", '')
-        ce_tensor = hn.tl.compute_ce_tensor(self.expressed_lr_df, self.w_best,
+            with open("output/ce_tensor_"+self.name+".pkl", 'rb') as f:
+                ce_tensor = pickle.load(f)
+        else:
+            ce_tensor = hn.tl.compute_ce_tensor(self.expressed_lr_df, self.w_best,
                                                  self.elements_expr_df_dict, self.dataset)
+            with open('output/ce_tensor_'+self.name+".pkl"):
+                pickle.dump(ce_tensor, f)
         #We can then filter the edges with low specifities
         if path.exists("output/filtered_ce_tensor_"+self.name+".pkl"):
             with open("output/filtered_ce_tensor_"+self.name+".pkl", 'rb') as f:
