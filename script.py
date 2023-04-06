b5902ee50261d324d288be81ecd021737579d8fc
Sbrussee <Siemenbrussee@hotmail.com>
2023-04-06 13:27:29 +0200

    yes

--- script.py
+++ script.py
@@ -410,7 +410,7 @@ elif args.dataset == 'nanostring':
     #Pass each through holonet
     for dataset in [normal, cancer]:
         dataset.obs['cell_type'] = dataset.obs['cellType']
-        dataset.obs['predicted_cell_type'] = dataset.obs['cellType']
+        dataset.obsm['predicted_cell_type'] = dataset.obs['cellType']
         holonet_pipeline(dataset, organism, name="Nanostring_"+str(dataset.obs['Run_Tissue_name'].unique()[0]),
          list_of_target_lr=[], list_of_target_genes=[])
 
